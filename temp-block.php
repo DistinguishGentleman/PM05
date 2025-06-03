7a885c989642de06ab3b9c09ac33a464ec337ef5
DistinguishGentleman <vasiliyhinin@gmail.com>
2025-06-03 15:19:15 +0300

    Delete temp-block.php

--- temp-block.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once __DIR__."/Helper/head/head3.php";
-include_once __DIR__."/Helper/body/body_t-bl.php";
-include_once __DIR__."/Helper/loader/loader.php";
-include_once __DIR__."/Helper/script/script_t-bl.php";
-
-?>
-
