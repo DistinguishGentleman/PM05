62f1d6e07a5e9c4ddb231d0ef90b0bf46723a5bd
DistinguishGentleman <vasiliyhinin@gmail.com>
2025-06-03 15:18:19 +0300

    Delete about.php

--- about.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once __DIR__."/Helper/head/head1.php";
-include_once __DIR__."/Helper/body/body_about.php";
-include_once __DIR__."/Helper/loader/loader.php";
-include_once __DIR__."/Helper/script/script.php";
-
-?>
