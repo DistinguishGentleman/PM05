8465e998da25d08602837a0db8defbbdd0289001
DistinguishGentleman <vasiliyhinin@gmail.com>
2025-06-03 15:18:49 +0300

    Delete post-action.php

--- post-action.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once __DIR__."/Helper/head/head2.php";
-include_once __DIR__."/Helper/body/body_p-act.php";
-include_once __DIR__."/Helper/loader/loader.php";
-include_once __DIR__."/Helper/script/script.php";
-
-?>
-
