d854e7a1e38797dcae8f6195933b724d4efe1373
DistinguishGentleman <vasiliyhinin@gmail.com>
2025-06-03 15:19:02 +0300

    Delete posts.php

--- posts.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once __DIR__."/Helper/head/head1.php";
-include_once __DIR__."/Helper/body/body_posts.php";
-include_once __DIR__."/Helper/loader/loader.php";
-include_once __DIR__."/Helper/script/script.php";
-
-?>
-
