89478a180698913e8a2f391f49f6e9dbbf5efbd8
DistinguishGentleman <vasiliyhinin@gmail.com>
2025-06-03 15:19:09 +0300

    Delete register.php

--- register.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once __DIR__."/Helper/head/head2.php";
-include_once __DIR__."/Helper/body/body_reg.php";
-include_once __DIR__."/Helper/loader/loader.php";
-include_once __DIR__."/Helper/script/script_reg.php";
-
-?>
-
-
